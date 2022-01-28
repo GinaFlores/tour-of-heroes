a679eaea75a59be92b1bb932d07ecebfb4e524d2
Gina Gonzales Flores <floresgina2020@gmail.com>
2022-01-28 15:15:31 -0500

    add style mobile responsive nav

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  faBars = faBars;
+  faTimes = faTimes;
 
   constructor() { }
 
