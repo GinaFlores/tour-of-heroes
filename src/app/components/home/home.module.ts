f7408f55be93c64233699848b5730603ff1d67dd
KeylaMia <keylamialujimenezg@gmail.com>
2022-01-28 01:13:50 -0500

    add a styles

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -1,3 +1,4 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
@@ -19,7 +20,8 @@ import { HomeComponent } from "./home.component";
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
-        HomeRouteModule
+        HomeRouteModule,
+        FontAwesomeModule
     ]
 })
 export class HomeModule{}
